a93391b587308af1b1c8029d00fd55a1cbde7abc
tripleCC <triplec.linux@gmail.com>
2017-10-31 18:03:14 +0800

    initial commit

---
+++ lib/cocoapods/authors.rb
@@ -0,0 +1,7 @@
+require "cocoapods/authors/version"
+
+module Cocoapods
+  module Authors
+    # Your code goes here...
+  end
+end
---
+++ lib/cocoapods/authors/version.rb
@@ -0,0 +1,5 @@
+module Cocoapods
+  module Authors
+    VERSION = "0.1.0"
+  end
+end
